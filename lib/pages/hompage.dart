8979597ce8b1d6cd493ae5e0439e22abc3e8fdbd
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-05-21 07:17:54 +0500

    azaz

--- lib/pages/hompage.dart
+++ lib/pages/hompage.dart
@@ -3,6 +3,7 @@ import 'package:examui/class/most_interested.dart';
 import 'package:examui/class/popular.dart';
 import 'package:examui/models/product.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class Hompage extends StatefulWidget {
   const Hompage({super.key});
@@ -19,8 +20,7 @@ class _HompageState extends State<Hompage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding:
-                const EdgeInsets.only(top: 40, left: 25, right: 25, bottom: 25),
+            padding: const EdgeInsets.only(top: 40, left: 25, right: 25, bottom: 25),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -42,10 +42,7 @@ class _HompageState extends State<Hompage> {
                         ),
                         Text(
                           "Besnik Doe,",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500),
+                          style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.w500),
                         )
                       ],
                     )
@@ -109,35 +106,23 @@ class _HompageState extends State<Hompage> {
                   padding: EdgeInsets.all(27),
                   width: 270,
                   height: 165,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      image: DecorationImage(
-                          image: AssetImage("images/afterlogin/interior.png"),
-                          fit: BoxFit.cover)),
+                  decoration: const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)), image: DecorationImage(image: AssetImage("images/afterlogin/interior.png"), fit: BoxFit.cover)),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       const Text(
                         "25% Discount",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 18),
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 18),
                       ),
                       const Text(
                         "For a cozy yellow set!",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w300,
-                            fontSize: 15),
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.w300, fontSize: 15),
                       ),
-                    const  SizedBox(
+                      const SizedBox(
                         height: 10,
                       ),
                       ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: WidgetStateProperty.all<Color>(
-                                  Color(0xFF0c8a7b))),
+                          style: ButtonStyle(backgroundColor: WidgetStateProperty.all<Color>(Color(0xFF0c8a7b))),
                           onPressed: () {},
                           child: const Text("Learn More",
                               style: TextStyle(
@@ -154,35 +139,23 @@ class _HompageState extends State<Hompage> {
                   padding: EdgeInsets.all(20),
                   width: 270,
                   height: 165,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      image: DecorationImage(
-                          image: AssetImage("images/afterlogin/interior2.png"),
-                          fit: BoxFit.cover)),
+                  decoration: const BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20)), image: DecorationImage(image: AssetImage("images/afterlogin/interior2.png"), fit: BoxFit.cover)),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       const Text(
                         "35% Discount",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 18),
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 18),
                       ),
                       const Text(
                         "For a cozy yellow set!",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w300,
-                            fontSize: 15),
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.w300, fontSize: 15),
                       ),
                       const SizedBox(
                         height: 10,
                       ),
                       ElevatedButton(
-                          style: ButtonStyle(
-                              backgroundColor: WidgetStateProperty.all<Color>(
-                                  Color(0xFF0c8a7b))),
+                          style: ButtonStyle(backgroundColor: WidgetStateProperty.all<Color>(Color(0xFF0c8a7b))),
                           onPressed: () {},
                           child: const Text("Shop Now",
                               style: TextStyle(
@@ -194,21 +167,78 @@ class _HompageState extends State<Hompage> {
               ],
             ),
           ),
-          Row(
-            children: [
-              ElevatedButton(
-                  style: ButtonStyle(),
-                  onPressed: () {},
-                  child: Row(
-                    children: [
-                      Image.asset("images/afterlogin/iconsofa.png"),
-                      const Text(
-                        "Armchair",
-                        style: TextStyle(),
-                      )
-                    ],
-                  )),
-            ],
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 20),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color>(Color(0xFF0c8a7b)),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        children: [
+                          Image.asset("images/afterlogin/iconsofa.png"),
+                          Gap(10),
+                          const Text(
+                            "Armchair",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      )),
+                  Gap(10),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color>(Color(0xFF0c8a7b)),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        children: [
+                          Image.asset("images/afterlogin/iconsofa2.png"),
+                          Gap(10),
+                          const Text(
+                            "Armchair",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      )),
+                  Gap(10),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color>(Color(0xFF0c8a7b)),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        children: [
+                          Image.asset("images/afterlogin/iconbed.png"),
+                          Gap(10),
+                          const Text(
+                            "Armchair",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      )),
+                  Gap(10),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color>(Color(0xFF0c8a7b)),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        children: [
+                          Image.asset("images/afterlogin/iconlast.png"),
+                          Gap(10),
+                          const Text(
+                            "Armchair",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      )),
+                ],
+              ),
+            ),
           ),
           const SizedBox(
             height: 20,
@@ -241,11 +271,7 @@ class _HompageState extends State<Hompage> {
                 for (var i = 0; i < 10; i++)
                   InkWell(
                       onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (ctx) => MostInInside(
-                                    SingleProducts.products[i % 6])));
+                        Navigator.push(context, MaterialPageRoute(builder: (ctx) => MostInInside(SingleProducts.products[i % 6])));
                       },
                       child: MostInterested(SingleProducts.products[i % 6]))
               ],
@@ -275,14 +301,7 @@ class _HompageState extends State<Hompage> {
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
-              children: [
-                for (var i = 0; i < 10; i++)
-                  i % 2 == 0
-                      ? Popular("greystol", "Swoon Lounge", "Regal Do Lobo",
-                          "\$136.79")
-                      : Popular("sariqstol", "Swoon Lounge", "Regal Do Lobo",
-                          "\$283.39")
-              ],
+              children: [for (var i = 0; i < 10; i++) i % 2 == 0 ? Popular("greystol", "Swoon Lounge", "Regal Do Lobo", "\$136.79") : Popular("sariqstol", "Swoon Lounge", "Regal Do Lobo", "\$283.39")],
             ),
           )
         ],
